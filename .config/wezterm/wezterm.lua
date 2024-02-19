afb5ce8ee4201cb6353281324d5a8ce686c0b91d
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2024-02-20 00:38:32 +1100

    feat: add tmux conf

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,12 @@ config.color_scheme = "catppuccin-frappe"
 config.font_size = 15
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+config.keys = {
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+}
 
 -- inactive config
 --config.font = wezterm.font_with_fallback({
