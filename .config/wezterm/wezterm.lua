a55b91a6065d248a8a93da2c7b83b6db2d952657
Tom Brereton <tombrereton@Toms-MacBook-Pro.local>
2024-05-13 22:56:58 +1000

    feat: add keymaps for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,20 +1,38 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.color_scheme = "catppuccin-frappe"
 config.font_size = 15
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.keys = {
-	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
-	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "LeftArrow",  mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
 	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+	{ key = "=",          mods = "CMD", action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+	{ key = "+",          mods = "CMD", action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+	{ key = 'w',          mods = 'CMD', action = wezterm.action.CloseCurrentPane { confirm = true }, },
+	{ key = '\\',         mods = 'OPT', action = act.PaneSelect },
+	{
+		key = 'h',
+		mods = 'OPT',
+		action = act.ActivatePaneDirection 'Left',
+	},
+	{
+		key = 'l',
+		mods = 'OPT',
+		action = act.ActivatePaneDirection 'Right',
+	},
+	{
+		key = 'k',
+		mods = 'OPT',
+		action = act.ActivatePaneDirection 'Up',
+	},
+	{
+		key = 'j',
+		mods = 'OPT',
+		action = act.ActivatePaneDirection 'Down',
+	},
 }
-
--- inactive config
---config.font = wezterm.font_with_fallback({
---{ family = "Iosevka Term", weight = "Medium" },
---})
 
 return config
